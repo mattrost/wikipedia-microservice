4dd71a9f7632a216c36974e4d615f7f8cf68f49b
mattrost <71509176+mattrost@users.noreply.github.com>
2021-08-08 15:16:28 -0700

    Update wikipediasummary.py

--- wikipediasummary.py
+++ wikipediasummary.py
@@ -3,9 +3,18 @@ import wikipedia as wikipedia_module
 
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    welcome = wikipedia_module.summary('Wikipedia')
+    return welcome
+
 @app.route('/<data>/')
 def wikipedia(data):
-    summary = wikipedia_module.summary(data)
-    return summary
+    try:
+        summary = wikipedia_module.summary(data)
+        return summary
+    except:
+        error = 'Wikipedia search had errors, please try another search query.
+        return error
 
 app.run()
