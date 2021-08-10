2cfb392e4de78be80bf8ac80e051047fe87cd813
mattrost <71509176+mattrost@users.noreply.github.com>
2021-08-10 09:58:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import wikipedia as wikipedia_module
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-type'
 
 @app.route('/')
+@cross_origin()
 def welcome():
     welcome = 'Hello! Please enter a wikipedia search after the `.com/` to search for a Wikipedia summary.'
     return welcome
 
 @app.route('/<data>/')
+@cross_origin()
 def wikipedia(data):
     try:
         summary = wikipedia_module.summary(data)
